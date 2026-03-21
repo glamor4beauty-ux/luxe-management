21107772241afd5a061d98475514a4fad5883baf
clarence <glamor4beauty@gmail.com>
2026-03-21 23:38:45 +0000

    File changes

--- src/lib/AuthContext.jsx
+++ src/lib/AuthContext.jsx
@@ -133,7 +133,8 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={{ 
-      user, 
+      user,
+      setUser,
       isAuthenticated, 
       isLoadingAuth,
       isLoadingPublicSettings,
